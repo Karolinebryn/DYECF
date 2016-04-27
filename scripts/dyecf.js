aa0074e32bea30a54b7828272869e223750400c4
Karolinebryn <karolinebrynildsen@gmail.com>
2016-04-27 14:47:31 +0200

    refactoring js

--- scripts/dyecf.js
+++ scripts/dyecf.js
@@ -1,92 +1,102 @@
-var DYECF = {
-    context: null,
-    bgImage: null,
-    crossfiterImage: null,
-    crossfiter: {speed: 256, x: 0, y: 0},
-    bgReady: false,
-    crossfiterReady: false,
-    then: null,
-    keysDown: {},
-    init: function(container){
+var DYECFObj = DYECFObj || {};
+
+DYECFObj.crossfiter = function(){
+    this.speed = 256;
+    this.x = 0;
+    this.y = 0;
+    this.image = null;
+};
+
+DYECFObj.crossfiter.prototype.setImage = function(imagePath){
+    this.image = new Image();
+    this.image.src = imagePath;  
+};
+
+
+var DYECF = (function () {  
+    var context = null;
+    var crossfiter = null;
+    var bgImage = null;
+    var then = null;
+    var keysDown = {};
+    
+    var init = function(container){
         var canvas = document.createElement("canvas");
-        DYECF.context = canvas.getContext("2d");
+        context = canvas.getContext("2d");
         canvas.width = 512;
         canvas.height = 480;
         container.appendChild(canvas);
         
-        DYECF.initBackgroundImage();
-        DYECF.initCrossfiterImage();
-        DYECF.addEventListeners();
-    },
-    addEventListeners: function () {
+        initBackgroundImage();
+        initCrossfiter();
+        addEventListeners();
+    };
+    
+    var addEventListeners = function () {
         addEventListener("keydown", function(e){
-            DYECF.keysDown[e.keyCode] = true;
+            keysDown[e.keyCode] = true;
         }, false);
         
         addEventListener("keyup", function(e){
-            delete DYECF.keysDown[e.keyCode];
+            delete keysDown[e.keyCode];
         }, false);
-    },
-    initCrossfiterImage: function(){
-      DYECF.crossfiterImage = new Image();
-      DYECF.crossfiterImage.onload = function(){
-          DYECF.crossfiterReady = true;
-      }
-      
-      DYECF.crossfiterImage.src = "images/crossfiter.png";
-    },
-    initBackgroundImage: function() {
-        DYECF.bgImage = new Image();
-        DYECF.bgImage.onload = function(){
-            DYECF.bgReady = true;
-        };
-        
-        DYECF.bgImage.src = "images/background.png";
-    },
-    update: function(modifier){
-        if(38 in DYECF.keysDown){ //player holding up
-            DYECF.crossfiter.y -= DYECF.crossfiter.speed * modifier;
+    };
+    
+    var initCrossfiter = function(){
+        crossfiter = new DYECFObj.crossfiter();
+        crossfiter.setImage("images/crossfiter.png");
+    };
+    
+    var initBackgroundImage = function() {
+        bgImage = new Image();
+        bgImage.src = "images/background.png";
+    };
+    
+    var update = function(modifier){
+        if(38 in keysDown){ //player holding up
+            crossfiter.y -= crossfiter.speed * modifier;
         }
             
-        if(40 in DYECF.keysDown){ //player holding down
-            DYECF.crossfiter.y += DYECF.crossfiter.speed * modifier;
+        if(40 in keysDown){ //player holding down
+            crossfiter.y += crossfiter.speed * modifier;
         }
             
-        if(37 in DYECF.keysDown){ //player holding left
-            DYECF.crossfiter.x -= DYECF.crossfiter.speed * modifier;
+        if(37 in keysDown){ //player holding left
+            crossfiter.x -= crossfiter.speed * modifier;
         }
             
-        if(39 in DYECF.keysDown){ //player holding right
-            DYECF.crossfiter.x += DYECF.crossfiter.speed * modifier;
+        if(39 in keysDown){ //player holding right
+            crossfiter.x += crossfiter.speed * modifier;
         }         
-    },
-    render: function(){
-        if(DYECF.bgReady){
-            DYECF.context.drawImage(DYECF.bgImage, 0, 0);
-        }
-        
-        if(DYECF.crossfiterReady){
-            DYECF.context.drawImage(DYECF.crossfiterImage, DYECF.crossfiter.x, DYECF.crossfiter.y);
-        }
-    },
-    main: function(){
+    };
+    
+    var render = function(){
+        context.drawImage(bgImage, 0, 0);    
+        context.drawImage(crossfiter.image, crossfiter.x, crossfiter.y);
+    };
+    
+    var main = function(){
         var now = Date.now();
-        var delta = now - DYECF.then;
-        DYECF.update(delta/1000);
-        DYECF.render();
-        DYECF.then = now;
+        var delta = now - then;
+        update(delta/1000);
+        render();
+        then = now;
         //Request to do this again ASAP
-        DYECF.requestAnimationFrame(DYECF.main);
-    },
-    requestAnimationFrame: function(func1){
+        requestAnimationFrame(main);
+    };
+    
+    var requestAnimationFrame = function(func1){
         var w = window;
         requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;  
         requestAnimationFrame(func1);
-    },
-    startGame: function(container){
-        DYECF.init(container);
-        DYECF.then = Date.now();
-        DYECF.main();
-    }
-};
+    };
     
+    var DYECF = function(){};
+    DYECF.prototype.startGame = function(container){
+        init(container);
+        then = Date.now();
+        main();
+    };
+    
+    return DYECF;
+})();
